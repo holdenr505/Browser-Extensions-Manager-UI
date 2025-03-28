2b31ae3ca9bc20515f11d90bf27c047edfe2c86a
holdenr505 <holdenr505@gmail.com>
2025-03-28 19:44:45 -0400

    Prevent button focus when clicked

--- src/components/FilterBar.jsx
+++ src/components/FilterBar.jsx
@@ -1,11 +1,44 @@
+import { useState } from 'react'
+
 function FilterBar({ allHandler, activeHandler, inActiveHandler }) {
+    const [activeButton, setActiveButton] = useState('all');
+    
+    const handleAll = () => {
+        setActiveButton('all');
+        allHandler();
+    }
+
+    const handleActive = () => {
+        setActiveButton('active');
+        activeHandler();
+    }
+    
+    const handleInActive = () => {
+        setActiveButton('inactive');
+        inActiveHandler();
+    }
+
     return (
         <div className="filter-bar">
             <h2>Extensions List</h2>
             <div className="filter-buttons">
-                <button onClick={allHandler}>All</button>
-                <button onClick={activeHandler}>Active</button>
-                <button onClick={inActiveHandler}>Inactive</button>
+                <button
+                    className={activeButton === 'all' ? 'active-button' : ''}
+                    onClick={handleAll}
+                    onMouseDown={(event) => event.preventDefault()}>
+                        All
+                </button>
+                <button
+                    className={activeButton === 'active' ? 'active-button' : ''}
+                    onClick={handleActive}
+                    onMouseDown={(event) => event.preventDefault()}>
+                        Active</button>
+                <button
+                    className={activeButton === 'inactive' ? 'active-button' : ''}
+                    onClick={handleInActive}
+                    onMouseDown={(event) => event.preventDefault()}>
+                        Inactive
+                    </button>
             </div>
         </div>
     );
