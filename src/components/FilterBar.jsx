43c1cc43fa75e075712925be2658bb36b9d71a08
holdenr505 <holdenr505@gmail.com>
2025-03-27 21:43:22 -0400

    Update FilterBar.jsx

--- src/components/FilterBar.jsx
+++ src/components/FilterBar.jsx
@@ -1,11 +1,11 @@
-function FilterBar() {
+function FilterBar({ allHandler, activeHandler, inActiveHandler }) {
     return (
         <div className="filter-bar">
             <h2>Extensions List</h2>
             <div className="filter-buttons">
-                <button></button>
-                <button></button>
-                <button></button>
+                <button onClick={allHandler}>All</button>
+                <button onClick={activeHandler}>Active</button>
+                <button onClick={inActiveHandler}>Inactive</button>
             </div>
         </div>
     );
