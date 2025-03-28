2aea4a8f5bfb6a2011925d08a6dd16e6b5354d42
holdenr505 <holdenr505@gmail.com>
2025-03-27 20:36:18 -0400

    Add FilterBar component skeleton

---
+++ src/components/FilterBar.jsx
@@ -0,0 +1,14 @@
+function FilterBar() {
+    return (
+        <div className="filter-bar">
+            <h2>Extensions List</h2>
+            <div className="filter-buttons">
+                <button></button>
+                <button></button>
+                <button></button>
+            </div>
+        </div>
+    );
+}
+
+export default FilterBar;
